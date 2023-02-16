b2ad00314ad9d4333126249ee1a8a3a9ae56f3c3
tsrg <tsg@tmedia.fm>
2023-02-16 20:47:30 +0300

    chore: add constants for api

---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const watherApiURl = 'https://api.openweathermap.org/data/2.5/weather';
+export const geoApiURL = 'http://api.openweathermap.org/geo/1.0/direct';
+export const citySearchResponseLimit = 6;
