b5a92f3e16a90ea2e93ef6780300d2fbb1780c41
tsrg <tsg@tmedia.fm>
2023-02-19 18:38:24 +0300

    chore: modify consts

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,8 @@
 export const watherApiURl = 'https://api.openweathermap.org/data/2.5/weather';
 export const geoApiURL = 'http://api.openweathermap.org/geo/1.0/direct';
 export const citySearchResponseLimit = 6;
+export const geoSettings = {
+  enableHighAccuracy: true,
+  maximumAge        : 30000,
+  timeout           : 27000
+};
